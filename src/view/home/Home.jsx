02b424a8728d3751833591a8d32bab55fb61047e
aly <249813061@qq.com>
2022-03-08 18:59:36 +0800

    首页完毕

--- src/view/home/Home.jsx
+++ src/view/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Home.min.css'
 import hacker from '../../img/hacker.png'
 import spaceman from '../../img/spaceman.png'
+import background from '../../img/background.jpg'
 // import ufo from '../../img/ufo.png'
 // import annulus from '../../img/annulus.png'
 
@@ -19,18 +20,19 @@ export default function Home() {
 
     // 监听滚动时事件
     React.useEffect(() => {
-        const width = 3000 + hasVerticalScrolled
+        const width = 800 + hasVerticalScrolled
         // console.log(hasVerticalScrolled)
-        if (width >= 3000 && width <= 5000) {
+        if (width >= 800 && width <= 1300) {
             spacemanRef.current.style.width = width + 'px'
-            hackerRef.current.style.transform = `translateZ(${hasVerticalScrolled * 0.2}px)`
+            hackerRef.current.style.transform = `translateZ(${hasVerticalScrolled * 0.9}px)`
         } 
-        else if (hasVerticalScrolled >= 2350) {
+        else if (hasVerticalScrolled >= 600) {
             hackerRef.current.style.transform = `translateZ(0px)`
         }
     }, [hasVerticalScrolled])
     return (
         <div className='home'>
+            <img src={background} className="background" alt="" />
             <div className="head">
                 <img src={hacker} className="hacker" ref={hackerRef} alt="" />
                 <div className="spaceman" >
